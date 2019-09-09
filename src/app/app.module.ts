8677722fc3126cca1c6107c2e47ae88b11b03e9e
SimonDevelop <contact@simon-micheneau.fr>
2019-09-09 09:57:49 +0200

    feat: Firebase config, update angular and firsts methods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,35 +5,33 @@ import { HttpClientModule } from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 
 import { ReptileListComponent } from './reptile-list/reptile-list.component';
+import { ShowReptileComponent } from './reptile-list/show-reptile/show-reptile.component';
 import { ReptileFormComponent } from './reptile-list/reptile-form/reptile-form.component';
-import { ReptileShowComponent } from './reptile-list/reptile-show/reptile-show.component';
-
-import { TrackListComponent } from './reptile-list/track-list/track-list.component';
-import { TrackFormComponent } from './reptile-list/track-form/track-form.component';
 import { HeaderComponent } from './header/header.component';
 
-import { AuthService } from './services/auth.service';
 import { ReptilesService } from './services/reptiles.service';
+import { AuthService } from './services/auth.service';
 import { AuthGuardService } from './services/auth-guard.service';
 
 const appRoutes: Routes = [
   { path: 'auth/signin', component: SigninComponent },
   { path: 'reptiles', component: ReptileListComponent },
   { path: 'reptiles/new', component: ReptileFormComponent },
-  { path: 'reptiles/view/:id', component: ReptileShowComponent }
+  { path: 'reptiles/view/:id', component: ShowReptileComponent }
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
+    SignupComponent,
+    SigninComponent,
     ReptileListComponent,
+    ShowReptileComponent,
     ReptileFormComponent,
-    ReptileShowComponent,
-    TrackListComponent,
-    TrackFormComponent,
     HeaderComponent
   ],
   imports: [
---
+++ src/app/reptile-list/show-reptile/show-reptile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-show-reptile',
+  templateUrl: './show-reptile.component.html',
+  styleUrls: ['./show-reptile.component.scss']
+})
+export class ShowReptileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
