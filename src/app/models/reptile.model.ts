efd38353ea32161e4d62abff55451513de208a4d
SimonDevelop <contact@simon-micheneau.fr>
2019-09-10 11:21:27 +0200

    feat: Update reptile model + Layout show page

--- src/app/models/reptile.model.ts
+++ src/app/models/reptile.model.ts
@@ -1,5 +1,10 @@
 export class Reptile {
   photo: string
   note: string
-  constructor(public name: string, public birthday: string) { }
+  constructor(
+    public name: string,
+    public birthday: string,
+    public species: string,
+    public gender: string
+  ) { }
 }
